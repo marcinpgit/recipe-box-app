f3b5de62e4425c571a75ed1109db10f210f93bf1
marcinpgit <marcinp2012@gmail.com>
2018-07-27 15:31:00 +0200

    state added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Recipe Box App</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>{ this.props.appTitle }</h1>
+            </div>
+        );
+    }
 }
 
 export default Header;
--- src/components/RecipeApp.js
+++ src/components/RecipeApp.js
@@ -3,12 +3,26 @@ import React from 'react';
 import Header from './Header';
 
 class RecipeApp extends React.Component {
+    state = {
+        recipes: [
+            {
+                title: 'Zupa',
+                createBy: 'Alex',
+                description: 'Pyszna zupa dla glodnego',
+                ingredients: ['cebula', 'woda'],
+                directions: 'gotowac wode do 99 stopni'
+            }
+        ]
+    };
+
     render() {
+        const appTitle = 'Recipe Box Application';
+
         return (
             <div>
-                <Header />
+                <Header appTitle = { appTitle }/>
                 <div>
-                    My application
+                    My recipes:
                 </div>
             </div>
             
